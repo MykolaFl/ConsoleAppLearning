220ba7a3bb72815469046d84b3c618b77008e31c
Mykola.Florynskyi <Mykola.Florynskyi@MFLORYNSKYI-N2>
2023-05-26 01:18:01 +0300

    Add project files.

---
+++ ConsoleAppLearning/Exercise2.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleAppLearning
+//{
+//    internal class Exercise2
+//    {
+//        int number1, number2, temporary;
+
+//        Console.WriteLine("Input the First Number : ");
+//        number1 = Convert.ToInt32(Console.ReadLine());
+//        Console.WriteLine("Your Number : ");
+//        Console.WriteLine(number1);
+
+
+//        Console.WriteLine("Input the Second Number : ");
+//        number2 = Convert.ToInt32(Console.ReadLine());
+//        Console.WriteLine("Your Number : ");
+//        Console.WriteLine(number2);
+
+
+//        temporary = number1;
+//        number1 = number2;
+//        number2 = temporary;
+
+//        Console.WriteLine("Swapped Number 1 : {0}", number1);
+//        Console.WriteLine("Swapped Number 2 : {0}", number2);
+//    }
+//}
---
+++ ConsoleAppLearning/Exercise3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace ConsoleAppLearning
+//{
+//    internal class Exercise3
+//    {
+//        int number1, number2, number3, multiply;
+
+//        Console.WriteLine("Input the first number to multiply: ");
+//        number1 = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine("Input the second number to multiply: ");
+//        number2 = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine("Input the third number to multiply: ");
+//        number3 = Convert.ToInt32(Console.ReadLine());
+
+//        multiply = number1* number2 * number3;
+
+//        Console.WriteLine("The output of multiplication of three numbers = {0}", multiply);
+
+//    }
+//}
