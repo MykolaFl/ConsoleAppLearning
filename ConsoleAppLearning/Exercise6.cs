e18b5b6eebed94d0722225bab0a95d3a4cb45533
Mykola Florynskyi <nikolayflorynsky@gmail.com>
2023-06-02 01:20:38 +0300

    changeshw4

---
+++ ConsoleAppLearning/Exercise6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace ConsoleAppLearning
+//{
+//    public class Exercise6
+//    {
+//        Console.WriteLine("Input a number(integer): ");
+//            int j = Convert.ToInt32(Console.ReadLine());
+
+//        int sum = 0;
+
+//            while (j != 0)
+//            {
+//                sum += j % 10;
+//                j /= 10;
+//            }
+
+//    Console.WriteLine("Sum of the digits of the said integer: " + sum);
+//    }
+//}
--- ConsoleAppLearning/Program.cs
+++ ConsoleAppLearning/Program.cs
@@ -2,27 +2,71 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleAppLearning
 {
-    public class Exercise6
+    public class Homework4
     {
+
         public static void Main()
         {
-            Console.WriteLine("Input a number(integer): ");
-            int j = Convert.ToInt32(Console.ReadLine());
+            int mValue = FindMax(11, 7, 19);
+            Console.WriteLine("The largest value from three integer values: " + mValue);
+            Console.WriteLine("------------------------------------------");
+
+            int miValue = FindMin(11, 7, 19);
+            Console.WriteLine("The lowest value from three integer values: " + miValue);
+            Console.WriteLine("------------------------------------------");
+
+            int[] numbers = { 2, 4, 6, 8, 5 };
+            int sum = ComputeTheSumAnArray(numbers);
+            Console.WriteLine("The sum of all the elements of an array of integers: " + sum);
+            Console.WriteLine("------------------------------------------");
+
+            var array2 = new int[] { 4, 5, 22, 11, 7 };
+            int maxvalue = LargeValueFromArray(array2);
+            Console.WriteLine("The larger value from array - " + maxvalue);
+
+        }
+        public static int FindMax(int a, int b, int c) 
+        {
+            int maxValue = a;
+
+            if (b > maxValue) 
+                maxValue = b;
+
+            if (c > maxValue)
+                maxValue = c;
             
-            int sum = 0;
+            return maxValue;
+        }
+        public static int FindMin(int a, int b, int c)
+        {
+            int minValue = a;
 
-            while (j != 0)
+            if (b < minValue)
+                minValue = b;
+
+            if (c < minValue)
+                minValue = c;
+
+            return minValue;
+        }
+
+        public static int ComputeTheSumAnArray(int [] array)
+        {
+        int sum = 0;
+            for (int i = 0; i < array.Length; i++) 
             {
-                sum += j % 10;
-                j /= 10;
-            }
-
-            Console.WriteLine("Sum of the digits of the said integer: " + sum);
-
+                sum += array[i];
+            }    
+            return sum;
+        }
+        public static int LargeValueFromArray(int[] array2)
+        {
+            return array2.Max();
         }
     }
 }
