5904bbd86de727ce32458a21d2bfb30702a8f9d6
Mykola Florynskyi <nikolayflorynsky@gmail.com>
2023-06-06 01:39:35 +0300

    hw5

--- ConsoleAppLearning/Program.cs
+++ ConsoleAppLearning/Program.cs
@@ -7,66 +7,35 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppLearning
 {
-    public class Homework4
+    public class Homework5
     {
 
         public static void Main()
         {
-            int mValue = FindMax(11, 7, 19);
-            Console.WriteLine("The largest value from three integer values: " + mValue);
-            Console.WriteLine("------------------------------------------");
+            Student student = new Student("Mykola", "Hlibov", 18, "Kyiv");
+            student.Print();
 
-            int miValue = FindMin(11, 7, 19);
-            Console.WriteLine("The lowest value from three integer values: " + miValue);
-            Console.WriteLine("------------------------------------------");
+            Console.WriteLine();
 
-            int[] numbers = { 2, 4, 6, 8, 5 };
-            int sum = ComputeTheSumAnArray(numbers);
-            Console.WriteLine("The sum of all the elements of an array of integers: " + sum);
-            Console.WriteLine("------------------------------------------");
+            student.AddCourse("Math");
+            student.Print();
 
-            var array2 = new int[] { 4, 5, 22, 11, 7 };
-            int maxvalue = LargeValueFromArray(array2);
-            Console.WriteLine("The larger value from array - " + maxvalue);
+            Console.WriteLine();
+
+            student.DeleteCourse("Math");
+            student.Print();
+
+            Course course = new Course("English", "Ellyson Parker", 90);
+            course.Print();
+
+            Console.WriteLine();
+
+
+            course.AddStudent();
+            course.Print();
 
         }
-        public static int FindMax(int a, int b, int c) 
-        {
-            int maxValue = a;
 
-            if (b > maxValue) 
-                maxValue = b;
 
-            if (c > maxValue)
-                maxValue = c;
-            
-            return maxValue;
-        }
-        public static int FindMin(int a, int b, int c)
-        {
-            int minValue = a;
-
-            if (b < minValue)
-                minValue = b;
-
-            if (c < minValue)
-                minValue = c;
-
-            return minValue;
-        }
-
-        public static int ComputeTheSumAnArray(int [] array)
-        {
-        int sum = 0;
-            for (int i = 0; i < array.Length; i++) 
-            {
-                sum += array[i];
-            }    
-            return sum;
-        }
-        public static int LargeValueFromArray(int[] array2)
-        {
-            return array2.Max();
-        }
     }
 }
---
+++ ConsoleAppLearning/Student.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLearning
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string[] CoursesAttended { get; private set; }
+
+        public Student(string firstName, string lastName, int age, string city)
+            : this(firstName, lastName, age, city, new string[0])
+        {
+        }
+
+        public Student(string firstName, string lastName, int age, string city, string[] coursesAttended)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            City = city;
+            CoursesAttended = coursesAttended;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"City: {City}");
+            Console.WriteLine("Courses Attended:");
+            foreach (var course in CoursesAttended)
+            {
+                Console.WriteLine(course);
+            }
+        }
+
+        public void AddCourse(string courseName)
+        {
+            int newLength = CoursesAttended.Length + 1;
+            string[] updatedCourses = new string[newLength];
+            for (int i = 0; i < CoursesAttended.Length; i++)
+            {
+                updatedCourses[i] = CoursesAttended[i];
+            }
+            updatedCourses[newLength - 1] = courseName;
+            CoursesAttended = updatedCourses;
+        }
+
+        public void DeleteCourse(string courseName)
+        {
+            int index = Array.IndexOf(CoursesAttended, courseName);
+            if (index < 0)
+                return;
+
+            int newLength = CoursesAttended.Length - 1;
+            string[] updatedCourses = new string[newLength];
+            for (int i = 0, j = 0; i < CoursesAttended.Length; i++)
+            {
+                if (i != index)
+                {
+                    updatedCourses[j] = CoursesAttended[i];
+                    j++;
+                }
+            }
+            CoursesAttended = updatedCourses;
+        }
+
+    }
+}
